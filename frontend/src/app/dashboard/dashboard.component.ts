2ee515031857acd28ad112fb71b136d7e977ff86
matyssxd <matissp1302@gmail.com>
2024-06-20 23:21:10 +0300

    started working on dashboard design

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -8,20 +8,33 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   isLoggedIn: boolean = false;
+  selectedDate = new Date();
 
   constructor(private router: Router, private storageService: StorageService) {
     this.isLoggedIn = this.storageService.isLoggedIn();
 
     if (!this.isLoggedIn) {
-      this.router.navigate(["/login"]);
+      this.router.navigate(['/login']);
     }
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  changeDate(option: string): void {
+    switch (option) {
+      case 'increase':
+        this.selectedDate.setDate(this.selectedDate.getDate() + 1);
+        break;
+      case 'decrease':
+        this.selectedDate.setDate(this.selectedDate.getDate() - 1);
+        break;
+      default:
+        console.log('how did you get here');
+        break;
+    }
   }
 }
